0fa086b6b7f26c981673b140e6890e4158d06e40
CMD TØXÏC <promiseability845@gmail.com>
2026-05-24 15:35:17 +0000

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCp0KbklA4vyL-4LMSY3y_SFU_W8yCspgU",
+  authDomain: "c-licon-data-bank.firebaseapp.com",
+  projectId: "c-licon-data-bank",
+  storageBucket: "c-licon-data-bank.firebasestorage.app",
+  messagingSenderId: "499812180714",
+  appId: "1:499812180714:web:8aecff28017ccd38732d93",
+  measurementId: "G-SZCWWDRTHG"
+};
+
+firebase.initializeApp(firebaseConfig);
