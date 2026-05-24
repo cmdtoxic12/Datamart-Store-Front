25a6bb10395055c377d592dc63d7d7d0bbac22b2
CMD TØXÏC <promiseability845@gmail.com>
2026-05-24 15:40:45 +0000

    Implement authentication check and logout function

---
+++ protect.js
@@ -0,0 +1,15 @@
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
+
+function logout() {
+  auth.signOut().then(() => {
+    window.location.href = "login.html";
+  });
+}
